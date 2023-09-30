0faa486a221d4bff1cfdfc2c42bd9f99cca27973
gazi_fayaz <gazifayaz.16694.com>
2023-09-30 23:20:16 +0600

    create multi_model, make run_model and TRE as functions

--- TRE.py
+++ TRE.py
@@ -16,20 +16,24 @@ import torch
 from transformers import get_linear_schedule_with_warmup
 from transformers import BertForSequenceClassification, AdamW, BertConfig
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-best_model = torch.load("E:/University Courses/CSE400/project code/TRE/model_TRE.pt")
-# best_model.eval()
-text = "Just kind of feel numb, you know."
+def TRE(text:str):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-input_ids = torch.tensor(tokenizer.encode(text, add_special_tokens=True)).unsqueeze(0)
-with torch.no_grad():
-    # Assuming your model takes both text and audio inputs
-    output = best_model(input_ids)[0]
+    best_model = torch.load("E:/University Courses/CSE400/project code/TRE/model_TRE.pt")
+    # best_model.eval()
+    text = "Just kind of feel numb, you know."
 
-    # Process the model output to get predictions
-    # _, preds = torch.max(output, 1)
-    probabilities = torch.softmax(output, dim=1)
+    input_ids = torch.tensor(tokenizer.encode(text, add_special_tokens=True)).unsqueeze(0)
+    with torch.no_grad():
+        # Assuming your model takes both text and audio inputs
+        output = best_model(input_ids)[0]
+
+        # Process the model output to get predictions
+        # _, preds = torch.max(output, 1)
+        probabilities = torch.softmax(output, dim=1)
 
 
-print("Predicted label:", probabilities.tolist())
+    # print("Predicted label:", probabilities.tolist())
+    # tre_res=(probabilities.tolist())[0]
+    return (probabilities.tolist())[0]
---
+++ multi_model.py
@@ -0,0 +1,36 @@
+import multiprocessing
+from run_model import run_model # For TIM-Net
+from TRE import TRE 
+from multiprocessing import Process
+
+# if __name__ == "__main__":
+#     manager = multiprocessing.Manager()
+
+file_path = "E:/University Courses/CSE400/project code/Capstone-Project-Modified-TIM-Net_SER/Code/COMBINED/COMBINED/Disgust/F_01_OISHI_S_1_DISGUST_1.wav"
+
+text = "Just kind of feel numb, you know."
+
+timnet_res=run_model(file_path)
+tre_res = TRE(text)
+
+# p = Process(target=run_model, args=([file_path, timnet_res]))
+# q = Process(target=TRE, args=([text, tre_res]))
+
+# p.start()
+# q.start()
+
+# p.join()
+# q.join()
+
+# print(timnet_res)
+# print(tre_res)
+
+# Weighted Averaging
+accuracy_timnet = 0.7165
+accuracy_tre = 0.7394
+weight_timnet = accuracy_timnet/(accuracy_timnet+accuracy_tre)
+weight_tre = accuracy_tre/(accuracy_timnet+accuracy_tre)
+multi_model_res=[]
+for i in range(len(timnet_res)):
+    multi_model_res.append((timnet_res[i]*weight_timnet) + (tre_res[i]*weight_tre))
+print(multi_model_res)
